49febf1ecdae1f8d6f443d407575122b7152fb6e
Johao <165242050+5tuartS@users.noreply.github.com>
2025-11-04 14:30:28 -0500

    Update arista.h
    Actualizamos la clase Arista para incluir tipo de ruta (vehicular, peatonal, etc.) y estado (activa/inactiva). Así podemos diferenciar mejor las conexiones.

--- arista.h
+++ arista.h
@@ -1,23 +1,28 @@
-//#pragma once
+#pragma once
 #include <iostream>
+using namespace std;
 
 class Vertice;
 
-class Arista
-{
+class Arista {
   int peso;
+  string tipoRuta;
+  bool activa;
+
   Arista* sig;
   Vertice* dest;
 
   friend class Grafo;
 
 public:
-  Arista(Vertice* _dest, int peso);
+  Arista(Vertice* _dest, int _peso, string _tipoRuta, bool _activa);
 };
 
-Arista::Arista(Vertice* _dest, int _peso)
-{
+Arista::Arista(Vertice* _dest, int _peso, string _tipoRuta, bool _activa) {
   dest = _dest;
   peso = _peso;
+  tipoRuta = _tipoRuta;
+  activa = _activa;
   sig = NULL;
 }
+
